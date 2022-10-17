50995267dc4abd5382e0e9d1dd9d128a8ab45a3b
NXTMax <maxenceP25@gmail.com>
2022-10-17 17:19:19 +0200

    Objectified symptom counts writing

--- Project02Eclipse/src/com/hemebiotech/analytics/Program.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Program.java
@@ -1,29 +1,20 @@
 package com.hemebiotech.analytics;
 
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Map;
 
 public class Program {
 
     public static void main(String[] args) {
-        { //mocking the parameter for testing
+        { //mocking the parameters for testing
             args = new String[2];
             args[0] = "Project02Eclipse/symptoms.txt";
-            args[1] = "Project02Eclipse/result.out";
+            args[1] = "Project02Eclipse/results.out";
         }
         
         ReadSymptomDataFromFile symptomFileReader = new ReadSymptomDataFromFile(args[0]);
-        Map<String, Integer> symptomCounts = SymptomsCounter.countSymptomsFromList(symptomFileReader.GetSymptoms());
+        WriteSymptomCountsToFile symptomCountsFileWriter = new WriteSymptomCountsToFile(args[1]);
 
-        try {
-            FileWriter writer = new FileWriter(args[1]);
-            for (Map.Entry<String,Integer> symptomCount : symptomCounts.entrySet()) {
-                writer.write(symptomCount.toString() + '\n'); //Lucky me, the toString format for Map entries goes "key=value"
-            }
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Map<String, Integer> symptomCounts = SymptomsCounter.countSymptomsFromList(symptomFileReader.GetSymptoms());
+        symptomCountsFileWriter.PrintSymptoms(symptomCounts);
     }
 }
